b6065bbdc0e86e8f335b764a4f7bd1d9deddd8c3
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-09 09:57:40 +0200

    update path for public files.

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -3,7 +3,7 @@ import multer from "multer";
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, "public/assets");
+    callback(null, "../../public/assets");
   },
   filename(req, file, callback) {
     callback(null, file.originalname);
