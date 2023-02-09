3eadce37b31a4683eb4c5ec41bc4db2c52bfeb41
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-09 10:12:58 +0200

    Add login functionality.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -39,3 +39,28 @@ export const register = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      console.log("This email is not found", new Date());
+      return res
+        .status(400)
+        .json({ error: "Email or password are in correct" });
+    }
+    if (!(await bcrypt.compare(password, user.password))) {
+      console.log("This password is not correct", new Date());
+      return res
+        .status(400)
+        .json({ error: "Email or password are in correct" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    console.log("🚀 ~ file: auth.js:47 ~ login ~ error", error);
+    res.status(500).json({ error: error.message });
+  }
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js";
 import fileUpload from "../middlewares/fileUpload.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", fileUpload.single("picture"), register);
+authRouter.post("/login", login);
 
 export default authRouter;
