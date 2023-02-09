731f2197b2a5e8c0636c17b5437bcaff3f3942bb
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-09 09:55:55 +0200

    update path for public files.

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "../public/assets")));
 
 /* ROUTES */
 app.use("/api/v1", router);
